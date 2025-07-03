bf281e5d427c626d8b42a0f4725c346db4eb2a52
JamesTang <James-Heller@Outlook.com>
2025-07-03 13:35:30 +0800

    Just work

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,24 +1,17 @@
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import space.jamestang.ktimer.KTimerClient;
 import space.jamestang.ktimer.heartbeat.HeartbeatCallbackHandler;
 import space.jamestang.ktimer.message.HeartbeatData;
-import space.jamestang.ktimer.message.KTimerMessage;
 import space.jamestang.ktimer.message.enums.TimerPriority;
 
 import java.io.IOException;
-import java.util.function.Function;
 
 public class Main {
 
 
     public static void main(String[] args) throws IOException {
 
-        ObjectMapper mapper = new ObjectMapper().findAndRegisterModules();
-
-
-        KTimerClient client = getKTimerClient(mapper);
+        KTimerClient client = getKTimerClient();
         client.registerCallbackHandler(HeartbeatData.class, new HeartbeatCallbackHandler());
         client.registerCallbackHandler(SomeData.class, new SomeDataCallbackHandler());
         client.startAsync();
@@ -27,22 +20,7 @@ public class Main {
         client.scheduleTask("OID1", payload, 10000L, TimerPriority.NORMAL, null);
     }
 
-    private static KTimerClient getKTimerClient(ObjectMapper mapper) {
-        Function<KTimerMessage, byte[]> messageEncoder = data -> {
-            try {
-                return mapper.writeValueAsBytes(data);
-            } catch (JsonProcessingException e) {
-                throw new RuntimeException(e);
-            }
-        };
-        Function<byte[], KTimerMessage> messageDecoder = bytes -> {
-            try {
-                return mapper.readValue(bytes, KTimerMessage.class);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        };
-
-        return new KTimerClient(messageEncoder, messageDecoder, "localhost", 8080, "T1", "001", "Test");
+    private static KTimerClient getKTimerClient() {
+        return new KTimerClient("localhost", 8080, "T1", "001", "Test");
     }
 }
